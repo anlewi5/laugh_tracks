ac20621ebce84856ec3e8f6560d4829285207030
Anna Lewis <anna.noel.lewis@gmail.com>
2017-10-09 09:22:34 -0600

    remove remnant

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,5 +1,5 @@
 class LaughTracksApp < Sinatra::Base
-  
+
   get '/comedians' do
     if params[:age]
       @comedians = Comedian.where(age: params[:age])
@@ -8,9 +8,5 @@ class LaughTracksApp < Sinatra::Base
     end
     @specials = Special.all
     erb :index
-  end
-
-  get '/comedians?age={age}' do
-    erb :show
   end
 end
